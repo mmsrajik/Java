f3d4b029689697e38801756946fc667bc542d90b
RAJIK AHAMED M <mmsrajikahamed@gmail.com>
2019-07-07 01:44:52 +0530

    Add files via upload

---
+++ ValidateShuffledString.java
@@ -0,0 +1,100 @@
+
+Question :
+Validate Shuffled String
+
+There are 3 strings str1, str2 and str3 given as input. Str3 is said to be a shuffle of str1 and str2 if it can be formed by interleaving the characters of str1 and str2 in a way that maintains the left to right ordering of the characters from each string.
+Example:
+Input:
+str1="abc"
+str2="def"
+str3="dabecf" 
+Output:
+True
+Explanation:str3="dabecf" is a valid shuffle since it preserves the character ordering of the two strings.
+So, given these 3 strings write a code that detects whether str3 is a valid shuffle of str1 and str2. If str3 is valid shuffle then print "True", otherwise print "false".
+
+
+
+Runtime Input : 
+abc
+def
+dabecf
+Output : 
+True
+
+
+
+
+
+import java.util.*;
+public class ValidateShuffledString
+ 
+{
+	public static void main(String args[])
+	{
+	 Scanner sc=new Scanner(System.in);  
+String str1=sc.next();
+String str2=sc.next();
+String str3=sc.next();
+
+int cou=0,con1=0,con2=0;
+for(int i=0;i<str1.length();i++)
+{
+	for(int j=0; j<str3.length();j++)
+	{                                   
+	if(i>=str1.length())
+	{
+		break;
+	}
+		if(str1.charAt(i)==str3.charAt(j))
+		{                                                
+		cou++;
+		 
+		i++;
+		}
+		
+	}
+	if(cou==str1.length())
+	{
+		con1=1;
+	}
+	
+	cou=0;
+}
+	
+for(int i=0;i<str2.length();i++)
+{
+	for(int j=0; j<str3.length();j++)
+	{ 		                                          
+		if(i>=str2.length())
+	{
+		break;
+	}
+		if(str2.charAt(i)==str3.charAt(j))
+		{
+		cou++;	
+		i++;
+		}
+		
+	}
+	if(cou==str2.length())
+	{
+		con2=1;
+	}
+	
+	cou=0;
+}
+
+
+if(con1==1 && con2==1)
+{
+	System.out.println("True");
+}
+else
+{
+	System.out.println("false");
+}
+
+
+	}
+}
