5a4a890efaad4427a4e2cd29e05564e7f5c9d998
RAJIK AHAMED M <mmsrajikahamed@gmail.com>
2021-09-28 12:37:28 +0530

    Update lessHeightAdjacentStudentsDCA.java

--- DCA/lessHeightAdjacentStudentsDCA.java
+++ DCA/lessHeightAdjacentStudentsDCA.java
@@ -1,3 +1,8 @@
+A physical education teacher asks students to assemble in a straight line for the morning assembly. 
+In spite of repeated instructions, the students do not obey the teacher.
+Given an array of N number of arguments in which each element represents the height of the student in that position. 
+The task here is to find the number of students, only for students numbered 1 to N -1(a[1] to a[N-1]), whose height is less than the height of their adjacent students
+
 Given the heights of students in the queue. the task is to find the number of students,whose heights is less than the heights of their adjacent students
 
 Intput:
@@ -7,7 +12,32 @@ Intput:
 Output:
 2
 
-
+solution:     
+     
+import java.util.Scanner;
+public class lessHeightAdjacentStudentsDCA{
+    public static void main(String[] args)
+	{
+        Scanner sc = new Scanner(System.in);
+        Integer n = sc.nextInt();
+        Integer[] arr = new Integer[n];
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+        int count=0;
+        for (int i = 0; i < n-2; i++) {
+            if (arr[i] > arr[i+1] && arr[i+2] > arr[i+1])
+            {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
+     
+     
+ partially correct solution: may be for slightly different questions: 
 
 import java.util.*;
 public class lessHeightAdjacentStudentsDCA{
